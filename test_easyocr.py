e3265d175936f9c5b853f7d65d47b01ae32fc2d6
Julia Malolepsza <j.malolepsza@op.pl>
2025-08-09 23:06:16 +0200

    for folder

--- test_easyocr.py
+++ test_easyocr.py
@@ -2,20 +2,42 @@ import easyocr
 from pdf2image import convert_from_path
 import numpy as np
 import re
+import os
+# pdf_path = "/Users/juliamalolepsza/Downloads/PL_S0DRVRY_VP_01.pdf"
+# images = convert_from_path(pdf_path)
+#list of PIL images representing each page of pdf
 
-pdf_path = "/Users/juliamalolepsza/Downloads/PL_BAU51416_COC_03.pdf"
-images = convert_from_path(pdf_path)
+folder = "/Users/juliamalolepsza/Downloads/"
+reader = easyocr.Reader(['pl', 'en'], gpu=False)
+vin_lpn_map = {}
 
-reader = easyocr.Reader(['pl'], gpu=False)
+for file in os.listdir(folder):
+    if file.endswith(".pdf"):
+        pdf_path = os.path.join(folder, file)
+        images = convert_from_path(pdf_path)
 
-for page_num, image in enumerate(images):
-    print(f"\n--- Page {page_num + 1} ---")
-    results = reader.readtext(np.array(image))
+        temp_lpn = None
+        vin_found = None
 
-    if not results:
-        print("No text found on this page.")
-    else:
-        for _, text, _ in results:
-            print(text)
+        for page_num, image in enumerate(images):
+            results = reader.readtext(np.array(image))
+            all_text = " ".join([text for _, text, _ in results])
+
+            vin_match = re.search(r'(?<=\sE\s)\b[A-Z0-9]{17}\b', all_text)
+            lpn_match = re.search(r'(?<=\sA\s)[0-9A-Z]{1,3}[\s][0-9A-Z]{3,5}\b', all_text)
+
+            if lpn_match:
+                temp_lpn = lpn_match.group()
+
+            if vin_match:
+                vin = vin_match.group()
+                vin_last4 = vin[-4:]
+            
+            vin_lpn_map[vin_last4] = temp_lpn
+print(vin_lpn_map)
+
+            
+        
 
 
+   
